c38dc6ade6264460dc4da5f333e33226d9b0e448
emilaa <emilaa@code.edu.az>
2022-11-03 21:31:15 +0400

    Homework

---
+++ Partial views, Load more/Controllers/AccountController.cs
@@ -0,0 +1,156 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+using Partial_views__Load_more.Data;
+using Partial_views__Load_more.Models;
+using Partial_views__Load_more.Services.Interfaces;
+using Partial_views__Load_more.ViewModels.AccountViewModels;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Partial_views__Load_more.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IEmailService _emailService;
+        private readonly IFileService _fileService;
+
+        public AccountController(UserManager<AppUser> userManager, 
+            SignInManager<AppUser> signInManager, AppDbContext context, 
+            IEmailService emailService,
+            IFileService fileService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailService = emailService;
+            _fileService = fileService;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerVM);
+            }
+
+            AppUser user = new AppUser
+            {
+                Fullname = registerVM.Fullname,
+                UserName = registerVM.Username,
+                Email = registerVM.Email
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, registerVM.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+
+                return View(registerVM);
+            }
+
+            //await _signInManager.SignInAsync(user, false);
+
+            string token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            string link = Url.Action(nameof(ConfirmEmail), "Account", new { userId = user.Id, token },
+                Request.Scheme, Request.Host.ToString());
+
+            string path = "wwwroot/assets/templates/verify.html";
+            string body = string.Empty;
+            string subject = "Verify email";
+
+            body = _fileService.ReadFile(path, body);
+
+            body = body.Replace("{{link}}", link);
+            body = body.Replace("{{fullname}}", user.Fullname);
+
+            _emailService.Send(user.Email, subject, body, null);
+
+            return RedirectToAction(nameof(VerifyEmail));
+        }
+
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            if (userId is null || token is null) return BadRequest();
+
+            AppUser user = await _userManager.FindByIdAsync(userId);
+
+            if (user is null) return NotFound();
+
+            await _userManager.ConfirmEmailAsync(user, token);
+
+            await _signInManager.SignInAsync(user, false);
+
+            return RedirectToAction("Index", "Accessories");
+        }
+
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            AppUser user = await _userManager.FindByEmailAsync(loginVM.UsernameOrEmail);
+
+            if (user is null)
+            {
+                user = await _userManager.FindByNameAsync(loginVM.UsernameOrEmail);
+            }
+
+            if (user is null)
+            {
+                ModelState.AddModelError("", "Email or password is wrong!");
+                return View(loginVM);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Email or password is wrong!");
+                return View(loginVM);
+            }
+
+            await _signInManager.SignInAsync(user, false);
+
+            return RedirectToAction("Index", "Accessories");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Accessories");
+        }
+    }
+}
